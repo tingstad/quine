ef22fbc2353144adb7e2674f51f4c176d21491dc
Richard Tingstad <richard.tingstad@gmail.com>
2021-04-28 22:16:42 +0200

    Add quine2.sh
    b = backslash \
    s = single quote '
    n = new line \n

--- test.sh
+++ test.sh
@@ -6,6 +6,11 @@ if ! [ "$(cat quine.sh)" = "$(./quine.sh)" ]; then
     diff quine.sh <(./quine.sh)
     exit 1
 fi
+if ! [ "$(cat quine2.sh)" = "$(./quine2.sh)" ]; then
+    echo "Wrong" >&2
+    diff quine2.sh <(./quine2.sh)
+    exit 1
+fi
 
 echo OK
 
