106f941094cbf1fbade492c6c1ba02a528330b7e
Richard Tingstad <richard.tingstad@gmail.com>
2022-04-24 16:07:34 +0200

    Page 8 (outdated xref)
    3 digit objects

--- pdf.sh
+++ pdf.sh
@@ -49,18 +49,37 @@ main(){
 
         }
     }' quine.pdf
+
+    inplace quine.pdf sed -E '/^%START PAGE 8/,/^%FINISH PAGE/{
+        /^%/!d
+        /^%FINISH PAGE/{
+            i\
+'"$(declareobjs 79 | sed 's/$/\\/')"'
+
+        }
+    }' quine.pdf
+
+    inplace quine.pdf sed -E \
+    '/^%BEGIN PAGE 8/,/^%END PAGE 8/{
+        /^%/!d
+        /^%END PAGE 8/{
+            i\
+'"$(objectsencoded 95 107 | wordwrap | sed 's/$/\\/')"'
+
+        }
+    }' quine.pdf
 }
 
 objectsencoded()( from=$1; to=$2
     for objnum in $(seq $from $to) ; do
-        objencoded $objnum
+        objencoded $objnum "$(objdef $objnum)"
     done
 )
 
-objencoded()( nobjum=$1
-    src=$(objdef $objnum)
+objencoded()( objnum=$1 src="$2"
     printf '%s\n' "$src" | sed -E "
     /^[0-9]+ 0 /{
+      s/^([0-9])([0-9])([0-9]) 0 obj/3\1 0 R  2 0 R  3\2 0 R  2 0 R  3\3 0 R  2 0 R 0 obj/
       s/^([0-9])([0-9]) 0 obj/3\1 0 R  2 0 R  3\2 0 R  2 0 R 0 obj/
       s/^([0-9]) 0 obj/3\1 0 R  2 0 R 0 obj/
       s/ 0 obj << \/Length ([0-9])([0-9])/  6 0 R  2 0 R  3\1 0 R  2 0 R  3\2 0 R  2 0 R/
@@ -76,6 +95,20 @@ objdef()( num=$1
     sed -n '/^'$num' 0 obj << /,/^endstream endobj$/p' quine.pdf
 )
 
+declareobjs()( num=$1
+    i=94
+    while read -r line; do
+        len=${#line}
+        if [ $len -lt 1 ]; then continue; fi
+        case "$line" in
+            %*) continue ;;
+        esac
+        printf '%d 0 obj << /Length %d >> stream\n(%s)\nendstream endobj\n' $((i+=1)) $((len+2)) "$line"
+    done <<EOF
+    $(sed -n '/^'$num' 0 obj << /,/] >> endobj$/p' quine.pdf)
+EOF
+)
+
 wordwrap(){
     sed -En 'H;${
         g
@@ -88,11 +121,11 @@ wordwrap(){
     }' | tr -d \\n | awk -v max=65 '{
         s = $0
         while (length(s) > max) {
-            # R  9 0 R  52 O R
-            #        ^-------^ max: 8
-            i = index(substr(s, max-8), "R")
-            print(substr(s, 1, i+max-8))
-            s = substr(s, i+max-8)
+            # R  9 0 R  152 O R
+            #        ^--------^ max: 9
+            i = index(substr(s, max-9), "R")
+            print(substr(s, 1, i+max-9))
+            s = substr(s, i+max-9)
             sub(/^ +/, "", s)
         }
         print s
